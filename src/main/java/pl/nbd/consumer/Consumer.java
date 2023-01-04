232b0761304cf74ec5260340ff2ec4efe3b3433d
Magdalena Malinowska <228932@edu.p.lodz.pl>
2023-01-04 22:00:26 +0100

    fix consumer to do not receive duplicate messages

--- src/main/java/pl/nbd/consumer/Consumer.java
+++ src/main/java/pl/nbd/consumer/Consumer.java
@@ -55,50 +55,12 @@ public class Consumer extends AbstractMongoRepository {
         kafkaConsumer.subscribe(List.of(Topics.CLIENT_TOPIC));
     }
 
-    public void initConsumerGroup() {
-        consumerGroup = new ArrayList<>();
-
-        Properties consumerConfig = new Properties();
-        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, UUIDDeserializer.class.getName());
-        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, Topics.CONSUMER_GROUP_NAME);//dynamiczny przydział
-        //consumerConfig.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "clientconsumer");//statyczny przydział
-        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192,kafka2:9292,kafka3:9392");
-
-        super.initDbConnection();
-        recordCollection = mongoDatabase.getCollection("consumerRecords", Record.class);
-        recordCollection.drop();
-
-        for (int i = 0; i < 2; i++) {
-            KafkaConsumer<UUID, String> kafkaConsumer = new KafkaConsumer(consumerConfig);
-            kafkaConsumer.subscribe(List.of(Topics.CLIENT_TOPIC));
-            consumerGroup.add(kafkaConsumer);
-        }
-    }
-
-
-    private void deleteConsumerGroup() throws ExecutionException, InterruptedException {
-        Properties properties = new Properties();
-        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192,kafka2:9292,kafka3:9392");
-        try (Admin admin = Admin.create(properties)) {
-            DescribeConsumerGroupsResult describeConsumerGroupsResult = admin.describeConsumerGroups(List.of(Topics.CONSUMER_GROUP_NAME));
-            Map<String, KafkaFuture<ConsumerGroupDescription>> describedGroups = describeConsumerGroupsResult.describedGroups();
-            for (Future<ConsumerGroupDescription> group : describedGroups.values()) {
-                ConsumerGroupDescription consumerGroupDescription = group.get();
-                System.out.println(consumerGroupDescription);
-            }
-            admin.deleteConsumerGroups(List.of(Topics.CONSUMER_GROUP_NAME));
-        }
-    }
-
-
-
     public void consume(KafkaConsumer<UUID, String> consumer) {
         try {
             consumer.poll(0);
             Set<TopicPartition> consumerAssignment = consumer.assignment();
             System.out.println(consumer.groupMetadata().memberId() + " " + consumerAssignment);
-            consumer.seekToBeginning(consumerAssignment);
+            //consumer.seekToBeginning(consumerAssignment);
 
             Duration timeout = Duration.of(100, ChronoUnit.MILLIS);
             MessageFormat formatter = new MessageFormat("Konsument {5},Temat {0}, partycja {1}, offset {2, number, integer}, klucz {3}, wartość {4}");
